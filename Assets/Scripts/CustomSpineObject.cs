6a70d2232bb36112b6b77e698399122a2a0ecdb6
Brandon Harrison <bdn.hrsn@gmail.com>
2018-07-21 13:35:33 -0400

    Added left and right move anim

--- Assets/Scripts/CustomSpineObject.cs
+++ Assets/Scripts/CustomSpineObject.cs
@@ -8,13 +8,66 @@ public class CustomSpineObject : MonoBehaviour {
 
 	private SkeletonAnimation skeletonAnim;
 	private string idleAnim = "idle";
+	private string walkAnim = "walk";
+	bool inputLeft, inputRight, inputJump;
 
 	void Awake() {
-		skeletonAnim = GetComponent<SkeletonAnimation>();
+		skeletonAnim = GetComponent<SkeletonAnimation> ();
 	}
 
 	void Start() {
 
-		// skeletonAnim.AnimationState.SetAnimation(0, idleAnim, true);
+		skeletonAnim.AnimationState.SetAnimation(0, idleAnim, true);
+	}
+
+	void Update() {
+		UpdateCurrentInputValues();
+
+		if (inputLeft || inputRight) {
+			if (inputLeft && inputRight) {
+				skeletonAnim.AnimationState.SetAnimation(0, idleAnim, true);
+			} else {
+				if (inputLeft) {
+					FlipCharacter(InputDirection.Left);
+				} else if (inputRight) {
+					FlipCharacter(InputDirection.Right);
+				}
+				if (skeletonAnim.AnimationName != walkAnim) {
+					skeletonAnim.AnimationState.SetAnimation(0, walkAnim, true);
+				}
+			}
+		} else {
+			skeletonAnim.AnimationState.SetAnimation(0, idleAnim, true);
+		}
+	}
+
+	void FlipCharacter(InputDirection pDirection) {
+		Vector3 tScale = transform.localScale;
+		switch (pDirection) {
+			case InputDirection.Left:
+				tScale.x = -1;
+				break;
+			case InputDirection.Right:
+				tScale.x = 1;
+				break;
+			default:
+				break;
+		}
+		if (tScale != transform.localScale) {
+			transform.localScale = tScale;
+		}
+	}
+
+	void UpdateCurrentInputValues() {
+		
+		inputLeft = Input.GetKey("left");
+		inputRight = Input.GetKey("right");
+		inputJump = Input.GetKeyDown("space");
+	}
+
+	enum InputDirection {
+		Left,
+		Right,
+		Jump,
 	}
 }
