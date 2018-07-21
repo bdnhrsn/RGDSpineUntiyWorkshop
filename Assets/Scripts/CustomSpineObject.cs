9fc423a2fbb32b43c77aecb175ec5df405132af1
Brandon Harrison <bdn.hrsn@gmail.com>
2018-07-21 12:35:50 -0400

    Initial spine demo script

---
+++ Assets/Scripts/CustomSpineObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Spine;
+using Spine.Unity;
+
+public class CustomSpineObject : MonoBehaviour {
+
+	private SkeletonAnimation skeletonAnim;
+	private string idleAnim = "idle";
+
+	void Awake() {
+		skeletonAnim = GetComponent<SkeletonAnimation>();
+	}
+
+	void Start() {
+
+		// skeletonAnim.AnimationState.SetAnimation(0, idleAnim, true);
+	}
+}
